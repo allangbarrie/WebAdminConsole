abd41af61ea49e44dd4db322667397ffd5c2690a
allangbarrie <51610478+allangbarrie@users.noreply.github.com>
2024-05-13 20:43:20 +0100

    Login to manage

--- Controllers/LeaderBoardsController.cs
+++ Controllers/LeaderBoardsController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.CodeAnalysis.Elfie.Extensions;
