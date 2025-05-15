8f1ed9c106e29973620b41e8907c0ef14e8c03d5
allangbarrie <51610478+allangbarrie@users.noreply.github.com>
2025-05-15 22:08:03 +0100

    Mountain updates

---
+++ Controllers/MountainResultsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAdminConsole.Migrations;
+using WebAdminConsole.Models;
+using WebAdminConsole.ViewModels;
+
+namespace WebAdminConsole.Controllers
+{
+    
+    public class MountainResultsController : Controller
+    {
+        private readonly AppIdentityDbContext _context;
+
+        public MountainResultsController(AppIdentityDbContext context)
+        {
+            _context = context;
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = new List<MountainResultsViewModel>();
+
+            foreach (Team team in await _context.Team
+                .ToListAsync())
+            {
+                var thisTeam = await _context.LeaderBoard
+                    .Where(u => u.Equals(team))
+                    .ToListAsync();
+
+                var mountainTeam = new MountainResultsViewModel()
+                {
+                    TeamName = team.Name,
+                    Stage4 = thisTeam
+                    .Where(u => u.StageId == 4)
+                    .Select(u => u.Time)
+                    .FirstOrDefault(),
+                    Stage5 = thisTeam
+                    .Where(u => u.StageId == 5)
+                    .Select(u => u.Time)
+                    .FirstOrDefault(),
+                    Stage16 = thisTeam
+                    .Where(u => u.StageId == 16)
+                    .Select(u => u.Time)
+                    .FirstOrDefault(),
+                    Stage18 = thisTeam
+                    .Where(u => u.StageId == 18)
+                    .Select(u => u.Time)
+                    .FirstOrDefault()
+                };
+
+                mountainTeam.TotalTime = mountainTeam.Stage4 + 
+                    mountainTeam.Stage5 + mountainTeam.Stage16 + 
+                    mountainTeam.Stage18;
+
+            }
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ ViewModels/MountainResultsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using WebAdminConsole.Models;
+
+namespace WebAdminConsole.ViewModels
+{
+    public class MountainResultsViewModel
+    {
+        public int? Position { get; set; }
+
+        public string? TeamName { get; set; }
+
+        public TimeSpan Stage4 { get; set; }
+
+        public TimeSpan Stage5 { get; set; }
+
+        public TimeSpan Stage16 { get; set; }
+
+        public TimeSpan Stage18 { get; set; }
+
+        public TimeSpan TotalTime { get; set; }
+
+    }
+}
